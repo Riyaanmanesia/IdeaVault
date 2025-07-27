0321f9f3c0ed6a8fc9c90aede4544770db7851f1
Riyaan manesia <manesiariyaan5@gmail.com>
2025-07-27 12:33:49 +0530

    prepared for the deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,13 +12,20 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+
 app.use(express.json()); //this will parse json bodies
 app.use(rateLimiter);
 
@@ -30,6 +38,18 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+
+//We serve the react application using the same backend port 
+//if it is in the production
+//if visit any other url than api serve react app
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server started on Port:", PORT);
